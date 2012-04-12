f5e23d6a8b9668c4e32bfca174fd992deca103b1
Andy Grover <agrover@redhat.com>
2012-04-12 12:54:24 -0700

    Change compiled module name to 'lvm', fill in setup.py
    Signed-off-by: Andy Grover <agrover@redhat.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,10 @@ liblvm = Extension('liblvm',
                     sources = ['liblvm.c'],
                     libraries= ['lvm2app'])
 
-setup (name = 'liblvm',
+setup (name = 'lvm',
        version = '1.0',
        description = 'Python binding for liblvm2',
+       maintainer='Andy Grover',
+       maintainer_email='andy@groveronline.com',
+       url='http://github.com/agrover/python-lvm',
        ext_modules = [liblvm])
